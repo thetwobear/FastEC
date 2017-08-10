71ef7b5570d3b05eaef8ea439c9e3d1fee7ac237
thetwobear <370380156@qq.com>
2017-08-10 21:14:19 +0800

    增加加载Loader

---
+++ bigbear-core/src/main/java/com/bigbear/bigbear_core/ui/BigBearLoader.java
@@ -0,0 +1,71 @@
+package com.bigbear.bigbear_core.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.bigbear.bigbear_core.R;
+import com.bigbear.bigbear_core.util.DimenUtil;
+import com.wang.avi.AVLoadingIndicatorView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 熊猿猿 on 2017/8/10/010.
+ */
+@SuppressWarnings("unused")
+public class BigBearLoader {
+
+    /**
+     * 缩放比例
+     */
+    private static final int LOADER_SIZE_SCALE = 8;
+    /**
+     * 上下偏移比例
+     */
+    private static final int LOADER_OFFSET_SCALE = 10;
+
+    /**
+     * 统一管理
+     */
+    private static final ArrayList<AppCompatDialog> LOADERS = new ArrayList<>();
+
+    /**
+     * 默认loader
+     */
+    private static final String DEFAULT_LOADER = LoaderStyle.BallClipRotatePulseIndicator.name();
+
+    public static void showLoading(Context context, String type) {
+        final AppCompatDialog dialog = new AppCompatDialog(context, R.style.dialog);
+        final AVLoadingIndicatorView avLoadingIndicatorView = LoaderCreator.create(type, context);
+        dialog.setContentView(avLoadingIndicatorView);
+        int deviceWidth = DimenUtil.getSrceenWidth();
+        int deviceHeight = DimenUtil.getSrceenWidth();
+        final Window dialogWindow = dialog.getWindow();
+        if (dialogWindow != null) {
+            WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+            lp.width = deviceWidth / LOADER_SIZE_SCALE;
+            lp.height = deviceHeight / LOADER_SIZE_SCALE;
+            lp.height = lp.height + deviceHeight / LOADER_OFFSET_SCALE;
+            lp.gravity = Gravity.CENTER;
+        }
+        LOADERS.add(dialog);
+        dialog.show();
+    }
+
+    public static void showLoading(Context context) {
+        showLoading(context, DEFAULT_LOADER);
+    }
+
+    public static void stopLoading() {
+        for (AppCompatDialog dialog : LOADERS) {
+            if (dialog != null) {
+                if (dialog.isShowing()) {
+                    dialog.cancel();
+                }
+            }
+        }
+    }
+}
---
+++ bigbear-core/src/main/java/com/bigbear/bigbear_core/util/DimenUtil.java
@@ -0,0 +1,25 @@
+package com.bigbear.bigbear_core.util;
+
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+import com.bigbear.bigbear_core.app.BigBear;
+
+/**
+ * Created by 熊猿猿 on 2017/8/10/010.
+ */
+
+public class DimenUtil {
+
+    public static int getSrceenWidth() {
+        final Resources resources = BigBear.getApplication().getResources();
+        final DisplayMetrics dm = resources.getDisplayMetrics();
+        return dm.widthPixels;
+    }
+
+    public static int getSrceenHeight() {
+        final Resources resources = BigBear.getApplication().getResources();
+        final DisplayMetrics dm = resources.getDisplayMetrics();
+        return dm.heightPixels;
+    }
+}
